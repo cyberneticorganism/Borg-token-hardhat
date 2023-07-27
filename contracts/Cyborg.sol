49dad68a9acb81846da3aca8a07ae74c30f75ab3
Dillon <dilloncortez@gmail.com>
2023-07-26 20:56:32 -0500

    updated smart contract and moved uniswapV3Factory code to deploy script

--- contracts/Cyborg.sol
+++ contracts/Cyborg.sol
@@ -6,18 +6,27 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+
+
 
 contract Cyborg is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
     address payable devWallet;
     mapping(address => bool) private isExcludedFromTax;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     uint256 public buyTax = 0;
     uint256 public sellTax = 0;
     uint256 public constant MAX_TAX = 500; // Represents 5% (basis points)
+    address payable uniswapPair;
+    // ISwapRouter public immutable swapRouter;
+    
 
     constructor() ERC20("Cyborg", "BORG") ERC20Permit("Cyborg") {
+        //  IUniswapRouter public constant uniswapRouter = swapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+        // IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D); // Mainnet Uniswap V2 router address
+        // uniswapPair = payable(factory.createPool(address(this), address(0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6), 500));
         _mint(msg.sender, 214000000000000 * 10 ** decimals());
+
         devWallet = payable(msg.sender);
     }
 
@@ -66,6 +75,10 @@ contract Cyborg is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
         sellTax = _sellTax;
     }
 
+    function setUniswapPair(address payable _uniSwapPair) external onlyOwner {
+        uniswapPair = _uniSwapPair;
+    }
+
     function excludeFromTax(address _account) external onlyOwner {
         isExcludedFromTax[_account] = true;
     }
@@ -74,7 +87,12 @@ contract Cyborg is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
         isExcludedFromTax[_account] = false;
     }
 
-
+        // checks whether the transfer is a swap
+    function _isSwap(address sender_, address recipient_) internal view returns (bool result) {
+        if (sender_ == uniswapPair || recipient_ == uniswapPair) {
+            result = true;
+        }
+    }
 
     function transfer(address recipient, uint256 amount) public override returns (bool) {
         if (!isExcludedFromTax[recipient] && amount > maxPurchaseAmount()) {
@@ -83,12 +101,14 @@ contract Cyborg is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
 
         uint256 taxAmount = calculateBuyTax(amount);
         uint256 newAmount = amount - taxAmount;
-
-        super.transfer(recipient, newAmount);
-
-        if (taxAmount > 0) {
-          super.transfer(address(this), taxAmount);
-        }
+        if(_isSwap(msg.sender, recipient)) {
+            super.transfer(recipient, newAmount);
+            if (taxAmount > 0) {
+                super.transfer(devWallet, taxAmount);
+            }
+        } else {
+            super.transfer(recipient, amount);
+        } 
 
         return true;
     }
@@ -100,12 +120,16 @@ contract Cyborg is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
 
         uint256 taxAmount = calculateSellTax(amount);
         uint256 newAmount = amount - taxAmount;
+        if(_isSwap(msg.sender, recipient)) {
+            super.transferFrom(sender, recipient, newAmount);
+            if (taxAmount > 0) {
+                super.transferFrom(sender, devWallet, taxAmount);
+            }
 
-        super.transferFrom(sender, recipient, newAmount);
-
-        if (taxAmount > 0) {
-            super.transferFrom(sender, address(this), taxAmount);
+        } else {
+            super.transferFrom(sender, recipient, amount);
         }
+
 
         return true;
     }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,32 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+// import "https://github.com/Uniswap/uniswap-v3-periphery/blob/main/contracts/interfaces/ISwapRouter.sol";
+// import "https://github.com/Uniswap/v3-core/blob/main/contracts/interfaces/IUniswapV3Factory.sol";
+const uniswapSdk = require("@uniswap/v3-sdk")
 const hre = require("hardhat");
+const factoryContract = require("../artifacts/contracts/uniswapV3Factory.json");
 
 async function main() {
   // const initialSupply = 10000;
+  const kevsWallet = "0xd7360E1a1480476f52677265b38D9da26f808877"
+  const factoryAddress = "0x1F98431c8aD98523631AE4a59f267346ea31F984"
+  const wethAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+  const provider = new hre.ethers.providers.AlchemyProvider(network = "goerli", API_KEY);
+  const signer = new hre.ethers.Wallet(PRIVATE_KEY, provider);
+  
+  const factory = await hre.ethers.Contract(factoryAddress, factoryContract.abi, signer);
 
+  // factory.createPool()
+  // const factory = uniswapSdk.UniswapV3Factory();
+
+  
   const BorgToken = await hre.ethers.getContractFactory("Cyborg");
   const token = await BorgToken.deploy();
-
   await token.waitForDeployment();
+  // tokenAddress = token.address();
+  const poolAddress = await factory.createPool(token.target, wethAddress);
+  await token.setUniswapPair(poolAddress);
 
   const totalSupply = await token.totalSupply()
 
